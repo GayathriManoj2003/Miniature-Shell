91e09b3035e25334b2759421db9ad51cba6c40f4
vboxuser <vboxuser@Ubuntu.myguest.virtualbox.org>
2023-07-26 20:25:08 +0530

    First commit

---
+++ demo.cpp
@@ -0,0 +1,101 @@
+#include<iostream>
+#include<stdlib.h>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <sstream>
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <system>
+using namespace std;
+
+class Cat{
+    string myText;
+    string inp;
+    public:
+    void cat_command(string x)
+    {inp=x;
+        ifstream MyReadFile (inp);
+      
+        while (getline(MyReadFile, myText)) 
+        {
+             cout << myText;
+        }
+
+        MyReadFile.close();
+    };
+    void cat_command(string x,string y)
+    {inp=y;
+        if (x=="-n")
+            {
+                // cout<<"N"<<endl;
+                
+                ifstream MyReadFile (inp);
+                int i=1;
+                while (getline(MyReadFile, myText)) 
+                {
+                    cout <<i<< myText<<endl;
+                    i++;
+                }
+
+                MyReadFile.close();
+
+                    }
+        else
+            {cout<<"IN ELSE"<<endl;
+                ifstream MyReadFile (x);
+                int i=1;
+                ofstream write_file(y);
+                ifstream io;
+                while (getline(MyReadFile, myText)) 
+                {
+                    write_file<< myText<<endl;
+                    
+                }
+                
+                write_file.close();
+                MyReadFile.close();
+            }
+    }
+};
+int main()
+{vector <string> tokens;
+	string str;
+    // vector <string> t{' ','\t',};
+	cout << "Enter command \n";
+	getline(cin, str);
+	
+ stringstream check1(str);
+     
+    string intermediate;
+     
+    while(getline(check1, intermediate,' '))
+    {
+        tokens.push_back(intermediate);
+    }
+     
+    // Tokens
+    // for(int i = 0; i < tokens.size(); i++)
+    //     cout << tokens[i] << '\n';
+
+if (tokens[0]=="cat") //cat command
+{   cout<<"Here"<<endl;
+    Cat c1;
+    if (tokens.size()==2)
+    {
+        c1.cat_command(tokens[1]);
+    }
+    if (tokens.size()==3)
+    {
+        // cout<<444<<endl;
+    c1.cat_command(tokens[1],tokens[2]);
+
+    }
+        
+}
+
+
+	return 0;
+}
