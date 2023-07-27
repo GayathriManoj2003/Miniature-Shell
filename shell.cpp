6ec71892b0aa9445950c652e9278e2759e12ca14
GayathriManoj2003 <gayathrimanoj2103@gmail.com>
2023-07-27 16:14:01 +0530

    chdir implemented

--- shell.cpp
+++ shell.cpp
@@ -10,40 +10,89 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
+#include <cstdlib>
 #include <errno.h>
 using namespace std;
 
+class Tokens {
+    public:
+        vector <string> tokens;
+        Tokens(string);
+        string operator[](int index);
+        int num_args();
+        // void hi()
+        // {
+        //     cout << "hi " << endl;
+        // }
+};
+
+int Tokens::num_args() {
+    return tokens.size() - 1;
+}
+
 class Cat{
     string myText;
-    string inp;
+    string input;
     public:
     void cat_command(string x)
     {
-        inp=x;
-        ifstream catFile (inp);
-        if(!catFile) { 
-            cerr << "Error: file could not be opened" << endl;
-            exit(1);
+        input = x;
+        char current_dir[4096];
+        getcwd(current_dir, sizeof(current_dir));
+        char *str = new char[x.size() + 1];
+
+        ifstream catFile(input);
+        if(!catFile) {
+            cout << "cat: " + input + ": No such file or directory" << endl;
         }
-        while (getline( catFile, myText))
+        while(getline(catFile, myText))
         {
              cout << myText;
         }
+        // cout << endl;
         catFile.close();
     };
 };
 
-
-
-class Tokens {
-    vector <string> tokens;
+class ChangeDir{
     public:
-        Tokens(string);
-        string operator[](int index);
-        void hi()
+        void execute(string dir_path)
         {
-            cout << "hi " << endl;
+            // chdir(dir_path.c_str());+
+            
+            try {
+                std::filesystem::current_path(dir_path); //setting path
+            }
+            catch ( const std::filesystem::filesystem_error& ex) {
+                 cout << "Error: " << ex.what() << endl;
+            }
+            // const char* path = dir_path.c_str();
+
+            // const char* curDir = std::getenv("PWD");
+            // cout << curDir << endl;
+            // if (curDir == nullptr) {
+            //     std::cout << "Failed to get the current working directory" << std::endl;
+            // }
+            // else if (setenv("PWD", path, 1) != 0) {
+            //     std::cout << "Failed to change directory" << std::endl;
+            // }
+        };
+};
+
+class Clear{
+    public:
+        Clear() {
         }
+        void execute( Tokens& obj)
+        {
+            if( obj.num_args() > 0 )
+            {
+                cout << "clear does not accept any arguments." << endl;
+                return;
+            }
+            // ANSI Escape Sequences used
+            cout << "\033[2J\033[1;1H";
+        };
 };
 
 class Fork {
@@ -59,7 +108,7 @@ class Fork {
             else if(!p)
             {
                 cout << "\nInside Child process.";
-                obj.hi();
+                // obj.hi();
                 // char * c = (obj[0]).c_str();
                 // char * c = (obj[0]).data();
                 // char * c = &(obj[0]);
@@ -74,44 +123,73 @@ class Fork {
 
 
 Tokens::Tokens(string cmd) {
-    stringstream stream(cmd);
-    string intermediate;
 
-    while( getline(stream, intermediate, ' ') )
-    {
-        tokens.push_back(intermediate);
+    string tok;
+    stringstream cmd_stream(cmd);
+
+    while( cmd_stream >> tok ) {
+        tokens.push_back(tok);
     }
 }
 string Tokens::operator[]( int index) {
     return tokens[index];
 } 
+
+class SimpleShell {
+    const string prompt = " $ ";
+
+    public:
+        void printPrompt() {
+            char current_dir[4096];
+            getcwd(current_dir, sizeof(current_dir));
+            // const char* current_dir = std::getenv("PWD");
+
+            cout << "\e[1m" << current_dir << prompt << "\e[0m";
+        }
+        void execute() {
+            int flag = 1;
+            while(flag) {
+                string cmd;
+                printPrompt();
+                getline(cin, cmd);
+                flag = executeCommand(cmd);
+            }
+        }
+        int executeCommand(string cmd) {
+
+            
+            Tokens tokens(cmd);
+            if (tokens[0]=="cat")
+            {
+                Cat().cat_command(tokens[1]);
+            }
+            else if (tokens[0].rfind( "./", 0) == 0) // execute program
+            {
+                Fork().execute(tokens);
+            }
+            else if (tokens[0] == "cd") // cd command
+            {
+                ChangeDir().execute(tokens[1]);
+            }
+            else if (tokens[0] == "clear") // clear command
+            {
+                Clear().execute(tokens);
+            }
+            else if (tokens[0]=="exit") // exit command
+            {
+                return 0;
+            }
+            else
+            {
+                cout << "Command " + tokens[0] + " not found." << endl;
+            }
+            return 1;
+        }
+
+};
 int main()
 {
-    string prompt(" $ ");
-    string cwd;
-    while(true) {
-        char current_dir[4096];
-        string str;
-        getcwd(current_dir, sizeof(current_dir));
-        cout << current_dir;
-        cout << prompt;
-        getline(cin, str);
-
-        Tokens tokens(str);
-        if (tokens[0]=="cat")
-        {
-            Cat().cat_command(tokens[1]);
-        }
-        else if (tokens[0].rfind( "./", 0) == 0) //execute program
-        {
-            Fork().execute(tokens);
-        }
-        else if (tokens[0]=="exit") //exit command
-        {
-            exit(0);
-        }
-
-    }
-
+    SimpleShell shell;
+    shell.execute();
 	return 0;
 }
